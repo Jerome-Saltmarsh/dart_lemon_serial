7cec71027af0704a0f0557d2e9ffaaf877edd24b
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-05-05 15:32:16 +1000

    1.0.1

---
+++ lib/byte_writer.dart
@@ -0,0 +1,55 @@
+
+import 'dart:convert' show utf8;
+import 'dart:typed_data';
+
+
+class ByteWriter {
+  var _index = 0;
+  final _buffer = Uint8List(50000);
+  final _buffers = <int, Uint8List>{};
+
+  List<int> writeToSendBuffer() {
+    final sendBuffer = _getSendBuffer();
+    for (var i = 0; i < _index; i++) {
+      sendBuffer[i] = _buffer[i];
+    }
+    _index = 0;
+    return sendBuffer;
+  }
+
+  void writeBool(bool value){
+    writeByte(value ? 1 : 0);
+  }
+
+  void writeString(String value){
+    writeInt(value.length);
+    if (value.length == 0) return;
+    final encoded = utf8.encode(value);
+    for(final character in encoded){
+      writeByte(character);
+    }
+  }
+
+  List<int> _getSendBuffer() {
+    final bufferIndex = _index ~/ 100;
+    final buffer = _buffers[bufferIndex];
+    if (buffer != null) return buffer;
+    final newBufferLength = (bufferIndex + 1) * 100;
+    final newBuffer = Uint8List(newBufferLength);
+    _buffers[bufferIndex] = newBuffer;
+    return newBuffer;
+  }
+
+  void writeInt(num value){
+    final abs = value.toInt().abs();
+    _buffer[_index] = (value >= 0 ? 100 : 0) + abs ~/ 100;
+    _buffer[_index + 1] = abs % 100;
+    _index += 2;
+  }
+
+  void writeByte(int value){
+    assert(value <= 256);
+    assert(value >= 0);
+    _buffer[_index++] = value;
+  }
+}
